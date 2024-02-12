4ad19f2f74219c0952854e8fd4e8d74943951e94
Jason Fan <jfanx86@gmail.com>
2024-02-11 16:14:35 -0800

    format codes

--- absl/hello_test.cc
+++ absl/hello_test.cc
@@ -1,13 +1,13 @@
-#include <string>
-#include "gtest/gtest.h"
 #include "hello.h"
+
+#include <string>
+
+#include "gtest/gtest.h"
 
 namespace hello {
 namespace {
 
-TEST(Greeter, Works) {
-  EXPECT_EQ("Hello tester", Greet("tester"));
-}
+TEST(Greeter, Works) { EXPECT_EQ("Hello tester", Greet("tester")); }
 
 }  // namespace
 }  // namespace hello
